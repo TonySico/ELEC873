3caf476f530531afb37553090296b046592ecbac
TonySico <anthony.sicoie@gmail.com>
2025-09-24 12:53:37 -0400

    [add] get rid of the excess stuff

--- assignment1/q6/mpi.c
+++ assignment1/q6/mpi.c
@@ -9,42 +9,56 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  int dSRec = size - rank;
-  int dSSend = dSRec - 1;
-  char data[dSRec];
+  int dataSize = size - rank;
+  char data[dataSize];
 
   int position = 0;
-  char sendBuf[dSSend];
-  int recData;
+  char sendBuf[dataSize];
+  char recData;
 
   if (rank == 0) {
-    for (int i = 0; i < size; i++)
-      data[i] = (char)i;
+    printf("dataBuff: ");
+    for (int i = 0; i < size; i++) {
+      data[i] = (char)(i + 1);
+      printf("%d, ", data[i]);
+    }
+    printf("\n");
 
-    MPI_Pack(data, dSSend, MPI_CHAR, &sendBuf, size, &position, MPI_COMM_WORLD);
-    MPI_Send(&sendBuf, position, MPI_PACKED, (rank + 1) % size, 0,
+    MPI_Pack(data, dataSize, MPI_CHAR, &sendBuf, dataSize * sizeof(char),
+             &position, MPI_COMM_WORLD);
+    MPI_Send(&sendBuf, position - 1, MPI_PACKED, (rank + 1) % size, 0,
              MPI_COMM_WORLD);
-    printf("Process %d kept %d and sent the rest to %d\n", rank, data[0],
-           (rank + 1) % size);
+    printf("Process %d recieved %d items, kept 1 (data = %d), and sent the "
+           "remaining %d items to %d\n",
+           rank, dataSize, data[0], position - 1, (rank + 1) % size);
   } else {
-    if (MPI_Recv(&data, dSRec, MPI_CHAR, (rank - 1 + size) % size, 0,
+    printf("Rank %d is expecting to recieve %d data\n", rank, dataSize);
+    if (MPI_Recv(&data, dataSize, MPI_CHAR, (rank - 1 + size) % size, 0,
                  MPI_COMM_WORLD, &status) == MPI_SUCCESS) {
-      MPI_Unpack(&data, dSRec, &position, &recData, 1, MPI_CHAR,
+      MPI_Unpack(data, dataSize, &position, &recData, 1, MPI_CHAR,
                  MPI_COMM_WORLD);
 
-      MPI_Unpack(&data, dSRec, &position, &sendBuf, dSSend, MPI_CHAR,
-                 MPI_COMM_WORLD);
+      if (rank != size - 1) {
+        MPI_Unpack(data, dataSize, &position, sendBuf, dataSize - 1, MPI_CHAR,
+                   MPI_COMM_WORLD);
 
-      MPI_Pack(&data, dSSend, MPI_CHAR, &sendBuf, dSSend, &position,
-               MPI_COMM_WORLD);
+        position = 0;
 
-      if (rank != size - 1)
+        MPI_Pack(data, dataSize - 1, MPI_CHAR, &sendBuf,
+                 (dataSize - 1) * sizeof(char), &position, MPI_COMM_WORLD);
+
+        printf("Rank %d is sending %d data\n", rank, dataSize);
         MPI_Send(&sendBuf, position - 1, MPI_PACKED, (rank + 1) % size, 0,
                  MPI_COMM_WORLD);
-
-      printf("Process %d recieved and kept %d from process %d and sent the "
-             "rest to %d\n",
-             rank, recData, status.MPI_SOURCE, rank + 1);
+        printf("Process %d recieved %d items from process %d, kept 1 (data = "
+               "%d), and sent the remaining %d items to %d\n",
+               rank, dataSize, status.MPI_SOURCE, recData, position - 1,
+               rank + 1);
+      } else {
+        printf("Process %d recieved %d items from process %d, kept 1 (data = "
+               "%d), and is the last process to recieve data\n",
+               rank, dataSize, status.MPI_SOURCE, recData);
+      }
     }
   }
 
