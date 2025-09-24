e0b3539528862f17d58804cbf3dc717523770a2a
TonySico <anthony.sicoie@gmail.com>
2025-09-24 11:59:20 -0400

    [add] MPI q6

--- assignment1/q6/mpi.c
+++ assignment1/q6/mpi.c
@@ -9,33 +9,38 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  char data[size - rank];
+  int dSRec = size - rank;
+  int dSSend = dSRec - 1;
+  char data[dSRec];
 
   int position = 0;
-  char sendBuf[size];
+  char sendBuf[dSSend];
+  int recData;
 
   if (rank == 0) {
     for (int i = 0; i < size; i++)
       data[i] = (char)i;
 
-    MPI_Pack(data, size, MPI_CHAR, &sendBuf, size, &position, MPI_COMM_WORLD);
+    MPI_Pack(data, dSSend, MPI_CHAR, &sendBuf, size, &position, MPI_COMM_WORLD);
     MPI_Send(&sendBuf, position, MPI_PACKED, (rank + 1) % size, 0,
              MPI_COMM_WORLD);
     printf("Process %d kept %d and sent the rest to %d\n", rank, data[0],
            (rank + 1) % size);
   } else {
-    if (MPI_Recv(&data, size - rank, MPI_CHAR, (rank - 1 + size) % size, 0,
+    if (MPI_Recv(&data, dSRec, MPI_CHAR, (rank - 1 + size) % size, 0,
                  MPI_COMM_WORLD, &status) == MPI_SUCCESS) {
-      MPI_Unpack(&data, size - rank, &position, void *outbuf, int outcount,
-                 MPI_Datatype datatype, MPI_Comm comm)
+      MPI_Unpack(&data, dSRec, &position, &recData, 1, MPI_CHAR,
+                 MPI_COMM_WORLD);
 
-          int recData = data[0];
+      MPI_Unpack(&data, dSRec, &position, &sendBuf, dSSend, MPI_CHAR,
+                 MPI_COMM_WORLD);
 
-      MPI_Pack(&data, size, MPI_CHAR, &sendBuf, size, &position,
+      MPI_Pack(&data, dSSend, MPI_CHAR, &sendBuf, dSSend, &position,
                MPI_COMM_WORLD);
 
       if (rank != size - 1)
-        MPI_Send(&rank, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+        MPI_Send(&sendBuf, position - 1, MPI_PACKED, (rank + 1) % size, 0,
+                 MPI_COMM_WORLD);
 
       printf("Process %d recieved and kept %d from process %d and sent the "
              "rest to %d\n",
