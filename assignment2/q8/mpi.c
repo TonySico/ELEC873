d84e093b3900cffc4bc34a7b0e92e2928dbd3ebe
TonySico <anthony.sicoie@gmail.com>
2025-10-23 15:16:25 -0400

    [q8]

--- assignment2/q8/mpi.c
+++ assignment2/q8/mpi.c
@@ -13,14 +13,17 @@
 #include <math.h>
 #include <mpi.h>
 #include <stdio.h>
-#include <stdlib.h>
+int rank;
 
-int epsilon(int new, int old) {
-  return (ceil(fabs(((((float)new - (float)old) / (float)old) / 100.0))));
+float epsilon(double new, double old) {
+  float e = ceil(fabs((new - old) / old * 100.0f));
+  if (!rank)
+    printf("%.8f e \n", e);
+  return (e);
 }
 
 int main(int argc, char *argv[]) {
-  int rank, size, n_runs = 10;
+  int size, n_runs = 10;
   MPI_Status status;
   double total_time = 0.0;
 
@@ -28,12 +31,10 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  int DATA_COUNT = atoi(argv[2]);
+  int DATA_COUNT = 0;
+  char data = '1';
 
-  char *data;
-  data = malloc(DATA_COUNT * sizeof(char));
-
-  float g[2] = {100.0, 1.0};
+  float g[2] = {10.0, 1.0};
 
   double offset = MPI_Wtime();
   double g_start = MPI_Wtime();
@@ -46,30 +47,37 @@ int main(int argc, char *argv[]) {
   while (epsilon(g[0], g[1]) > 1 && TAG) {
 
     // Set prev g value to current for new calculation
-    g[0] = g[1];
+    if (!rank) {
+      g[0] = g[1];
+    }
 
-    for (int i; i < n_runs; i++ && TAG) {
+    int i = 0;
+    while (i < n_runs && TAG) {
       if (!rank) {
         g_start = MPI_Wtime();
-        MPI_Send(data, DATA_COUNT, MPI_CHAR, 1, WORK, MPI_COMM_WORLD);
+        MPI_Send(&data, DATA_COUNT, MPI_CHAR, 1, WORK, MPI_COMM_WORLD);
       }
 
       if (rank) {
-        MPI_Recv(data, DATA_COUNT, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD,
+        MPI_Recv(&data, DATA_COUNT, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD,
                  &status);
         TAG = status.MPI_TAG;
       }
+      i++;
     }
 
     if (!rank) {
       g_end = MPI_Wtime();
       g[1] = (g_end - g_start - timer_overhead) / n_runs;
-      MPI_Recv(data, DATA_COUNT, MPI_CHAR, 1, MPI_ANY_TAG, MPI_COMM_WORLD,
+      printf("rank 0 waiting and gap = %.30f g_end = %.8f g_start = %.8f "
+             "timer_overhead = %.20f\n",
+             g[1], g_end, g_start, timer_overhead);
+      MPI_Recv(&data, DATA_COUNT, MPI_CHAR, 1, MPI_ANY_TAG, MPI_COMM_WORLD,
                &status);
     }
 
     if (rank) {
-      MPI_Send(data, DATA_COUNT, MPI_CHAR, 0, WORK, MPI_COMM_WORLD);
+      MPI_Send(&data, DATA_COUNT, MPI_CHAR, 0, WORK, MPI_COMM_WORLD);
     }
 
     n_runs *= 2;
@@ -77,7 +85,7 @@ int main(int argc, char *argv[]) {
 
   // Tells the mirror to stop waiting on a recv
   if (!rank) {
-    MPI_Send(data, DATA_COUNT, MPI_CHAR, 1, STOP, MPI_COMM_WORLD);
+    MPI_Send(&data, DATA_COUNT, MPI_CHAR, 1, STOP, MPI_COMM_WORLD);
   }
 
   if (!rank) {
